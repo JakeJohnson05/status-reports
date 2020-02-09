fa66410a6aafe2ee226ddce624eceed3b4582db7
Jake Johnson <jakejohnson801@outlook.com>
2020-02-08 18:05:56 -0700

    Error status reporter logs as an error

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const fullFormat = (symbol, message, extraIndent = '|', ...extra) => [
 	...extra.reduce((acc, cur) => [...acc, '\n', extraIndent, cur], [])
 ];
 
-const statusReporter = (chalkConfig, symbol, message, ...extra) => {
+const statusReporter = (logger, chalkConfig, symbol, message, ...extra) => {
 	const reporter = {};
 	const formattedMessage = fullFormat(symbol, chalkConfig(message), chalkConfig('|'), ...extra);
 
-	reporter.template = () => console.log(...formattedMessage);
+	reporter.template = () => logger(...formattedMessage);
 	reporter.template.fullMessage = formattedMessage.join(' ');
 
 	return reporter.template;
 };
 
-const success = (message, ...extra) => statusReporter(chalk.green, symbols.success, message, ...extra);
-const error = (message, ...extra) => statusReporter(chalk.red, symbols.error, message, ...extra);
-const warn = (message, ...extra) => statusReporter(chalk.yellow, symbols.warning, message, ...extra);
-const info = (message, ...extra) => statusReporter(chalk.blueBright, symbols.info, message, ...extra);
+const success = (message, ...extra) => statusReporter(console.log, chalk.green, symbols.success, message, ...extra);
+const error = (message, ...extra) => statusReporter(console.error, chalk.red, symbols.error, message, ...extra);
+const warn = (message, ...extra) => statusReporter(console.log, chalk.yellow, symbols.warning, message, ...extra);
+const info = (message, ...extra) => statusReporter(console.log, chalk.blueBright, symbols.info, message, ...extra);
 
 module.exports = {
 	success,
